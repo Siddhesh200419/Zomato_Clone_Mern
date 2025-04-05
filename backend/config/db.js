ee387993c6f65d8f51481f2899bf3ef77df1a165
Siddhesh <siddheshgite2004@gmail.com>
2025-04-05 14:55:04 +0530

    Fixed db config connection or made changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose, { connect } from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://siddheshgite2004:19082004@cluster0.dj11zgj.mongodb.net/food-del').then(() => console.log("DB connect"));
+    await mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB connect"));
 }
